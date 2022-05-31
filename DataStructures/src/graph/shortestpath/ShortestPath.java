2fda81102cf9d8837704da96645179c3e7b78c67
a <1179938183@qq.com>
2022-05-31 16:01:52 +0800

    最短距离：迪杰斯特拉算法（哈希表+有序表）

--- DataStructures/src/graph/shortestpath/ShortestPath.java
+++ DataStructures/src/graph/shortestpath/ShortestPath.java
@@ -4,6 +4,7 @@ import graph.Edge;
 import graph.Node;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 /**
@@ -11,63 +12,50 @@ import java.util.Map;
  */
 public class ShortestPath {
 
-    public void ShortestPath1(Node head){
+    /**
+     * 最短路径：迪杰斯特拉算法
+     * @param head
+     * @return 返回每个节点与起点的最短路径
+     */
+    public HashMap<Node, Integer> ShortestPath(Node head){
         // 每个点与起点的距离
         HashMap<Node, Integer> disMap = new HashMap<>();
         disMap.put(head, 0);
-        // 记录每个点与起点的最短距离
-        HashMap<Node, Integer> minDisMap = new HashMap<>();
+        // 记录已为最小距离的点
+        HashSet<Node> selectedNodes = new HashSet<>();
+        // 取出与起点最小距离且未被确定的点
+        Node minNode = getMinDisAndUnselectedNode(disMap, selectedNodes);
         while(!disMap.isEmpty()){
+            // 遍历该点所有的边
             for(Edge edge : head.edges){
-                if(disMap.containsKey(edge.to)){
-                    int weight = disMap.get(edge.from) + edge.weight;
-                    if(disMap.get(edge.to) > weight){
-                        disMap.put(edge.to, weight);
-                    }
-                }else{
-                    disMap.put(edge.to, disMap.get(edge.from) + edge.weight);
-
+                if(!disMap.containsKey(edge.to)){
+                    disMap.put(edge.to, disMap.get(minNode) + edge.weight);
                 }
+                disMap.put(edge.to, Math.min(disMap.get(edge.to), disMap.get(minNode) + edge.weight));
             }
-            minDisMap.put(head,disMap.remove(head));
-            int dis = Integer.MAX_VALUE;
-            // 找当前距离起点最小的点
-            for(Map.Entry<Node, Integer> m : disMap.entrySet()){
-                if(m.getValue() < dis){
-                    head = m.getKey();
-                    dis = m.getValue();
-                }
-            }
+            // 将该点添加为已选定
+            selectedNodes.add(minNode);
+            // 取出与起点最小距离且未被确定的点
+            minNode = getMinDisAndUnselectedNode(disMap, selectedNodes);
         }
+        return disMap;
     }
 
-    public void ShortestPath2(Node head){
-        // 每个点与起点的距离
-        HashMap<Node, Integer> disMap = new HashMap<>();
-        disMap.put(head, 0);
-        // 记录每个点与起点的最短距离
-        HashMap<Node, Integer> minDisMap = new HashMap<>();
-        while(!disMap.isEmpty()){
-            for(Edge edge : head.edges){
-                if(disMap.containsKey(edge.to)){
-                    int weight = disMap.get(edge.from) + edge.weight;
-                    if(disMap.get(edge.to) > weight){
-                        disMap.put(edge.to, weight);
-                    }
-                }else{
-                    disMap.put(edge.to, disMap.get(edge.from) + edge.weight);
-
-                }
-            }
-            minDisMap.put(head,disMap.remove(head));
-            int dis = Integer.MAX_VALUE;
-            // 找当前距离起点最小的点
-            for(Map.Entry<Node, Integer> m : disMap.entrySet()){
-                if(m.getValue() < dis){
-                    head = m.getKey();
-                    dis = m.getValue();
-                }
+    /**
+     * 取出与起点最小距离且未被确定的点
+     * @param disMap
+     * @param selectedNodes
+     * @return
+     */
+    public Node getMinDisAndUnselectedNode(HashMap<Node, Integer> disMap, HashSet<Node> selectedNodes){
+        Node minNode = null;
+        int minDis = Integer.MAX_VALUE;
+        for(Map.Entry<Node, Integer> m : disMap.entrySet()){
+            if(!selectedNodes.contains(m.getKey()) && minDis > m.getValue()){
+                minNode = m.getKey();
+                minDis = m.getValue();
             }
         }
+        return minNode;
     }
 }
